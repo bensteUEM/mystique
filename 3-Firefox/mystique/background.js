8b17b5ee12860c907075f3393ace1ea96c11f101
bensteUEM <GiPJa,15>
2017-04-28 11:31:38 +0200

    generated UA strings for #12

--- 3-Firefox/mystique/background.js
+++ 3-Firefox/mystique/background.js
@@ -82,3 +82,65 @@ function onError(error) {
 		openUrl(lastUrlOpened)
 	}
 }
+
+//========================= USER AGENT Part
+
+/*
+Map browser names to UA strings.
+*/
+function generateUA(){
+	//WINDOWS Platform
+	var winVersions = ["10.0","6.3","6.2","6.1","6.0","5.2","5.1","5.0"];
+	var windows = [];
+	for (version:winVersions){
+		windows.push("Windows NT "version+";");
+		windows.push("Windows NT "version+"; Win64; x64;");
+		windows.push("Windows NT "version+"; WOW64;");
+	}
+
+	//OSX Platform
+	var osxVersions = ["10.0","10.1","10.2","10.3","10.4","10.5","10.6","10.7","10.8","10.9","10.10","10.11","10.12"];
+	var apple = [];	
+	for (version:osxVersions){
+		apple.push("Macintosh; Intel Mac OS X "+version+";");
+		apple.push("Macintosh; PPC Mac OS X "+version+";");
+	}
+
+	//LINUX Platform
+	var linux = ["X11; Linux i686;","X11; Linux x86_64;","X11; Linux i686 on x86_64;","Maemo; Linux armv7l;","X11; Ubuntu; Linux i686;","X11; Ubuntu; Linux x86_64;","X11; Ubuntu; Linux i686 on x86_64;"];
+
+	//ANDROID Platform
+	var androidVersions = ["7.1","7.0","6.0","5.1","5.0","4.4","4.3","4.2","4.1"]	
+	var android = ["Android; Mobile","Android; Tablet"];
+	for (version:osxVersions){
+		android.push("Android "+version+"; Mobile;");
+		android.push("Android "+version+"; Tablet;");
+	}
+	
+	var allUseablePlatforms = [];
+	if (true) //TODO once personas do exist choose platform based on Persona #2 {
+		allUseablePlatforms = allUseablePlatforms.concat(windows);
+	if (true) {
+		allUseablePlatforms = allUseablePlatforms.concat(apple);
+	if (true) {
+		allUseablePlatforms = allUseablePlatforms.concat(linux);
+	if (true) {
+		allUseablePlatforms = allUseablePlatforms.concat(android);
+	}
+
+	var platform = 	allUseablePlatforms[Math.round(Math.random()*allUseablePlatforms.length)];
+
+	var geckoversions = ["18.0","18.1","26.0","28.0","30","32","34","37","44"] //all link a Firefox OS version
+	var rv_geckoversion = geckoversions[Math.round(Math.random()*geckoversions.length)];
+
+	var geckotrail = "Gecko/20100101" //20100101 for Desktops //TODO change for mobile #12 depending on #2
+	var firefoxversions = ["25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61"]
+	var firefoxversion = 	firefoxversions[Math.round(Math.random()*firefoxversions.length)];	
+
+	var generateUA = "Mozilla/5.0 (" + plaform + " rv:"+rv_geckoversion + ") "+ geckotrail + " Firefox/"+firefoxversion;
+
+	// COMPLETE UA Strings for special platforms
+	var iOSUA = ["Mozilla/5.0 (iPod touch; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) FxiOS/1.0 Mobile/12F69 Safari/600.1.4","Mozilla/5.0 (iPhone; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) FxiOS/1.0 Mobile/12F69 Safari/600.1.4","Mozilla/5.0 (iPad; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) FxiOS/1.0 Mobile/12F69 Safari/600.1.4"];
+
+	return generatedUA;
+}
