cf5a9c97608e23b6f1772795aa7a491325eea530
JuliKauf <jkaufmann23@googlemail.com>
2017-05-18 13:49:18 +0200

    Update  urlLib

--- 2-Chrome/libs/urlLib.js
+++ 2-Chrome/libs/urlLib.js
@@ -8,146 +8,155 @@
 
 var urlLib = {
 
-  // This is our main public function which returns the final approved url
-  generateURL: function(config) {
-    console.log("Received Config:");
-    console.log(config);
-    return new Promise((resolve, reject) => {
-      this._buildSearchString(config)
-        .then(this._getUrl)
-        .then(this._approveUrl)
-        .then((url) => {
-          if (url) {
-            resolve(url);
-            return;
-          }
-          resolve(this.generateURL(config));
-        }).catch((err) => {
-          console.log("Regenerate");
-          //return this.generateURL(config);
-        });
-    })
-
-  },
-
-  //------------- evolutionary algorithm------------------------------------------------------------------
-  //------------------------------------------------------------------------------------------------------
-
-  //-----fitness function--------------------------------------------------------------------------------
-  _evolutionModule: {
-    wordSorter: function(a, b) {
-      return a.score - b.score
-    },
-    updateLexicon : function(config) {      
-      return new Promise(function(resolve, reject) {
-        //TODO: find new words (score can be zero in the first iteration)
-        
-        var wordsWithScores = [{
-          name: "Geld",
-          score: 2
-        }, {
-          name: "Finanzen",
-          score: 0
-        }];
-        var personaKey = "Bank";
-        wordsWithScores.sort(urlLib._evolutionModule.wordSorter);
-       
-        return urlLib._evolutionModule.insertWordIfFitEnough("Holz", wordsWithScores, personaKey);
-      });
-    },
-    insertWordIfFitEnough : function(word, words, masterWord) {
-      return new Promise(function(resolve, reject) {
-        var getNumOfResults = function(search) {
-          return new Promise(function(resolve, reject) {
-            var url = "https://www.google.com/search?q=" + encodeURIComponent(search);            
-            $.get({
-              url: url,
-              success: function(data) {
-                var resultStatsRegexp = /<div id="resultStats">[^>]*</g;
-                var resultStats = resultStatsRegexp.exec(data)[0].replace("<div id=\"resultStats\">", "").replace("<", "").replace(" Ergebnisse", "").replace(" Results", "").replace("Ungefähr ", "");
-                var numberOfResults = parseInt(resultStats.replace(/\./g, ''));
-                return resolve(numberOfResults);
-              },
-              error: function(e) {
-                reject(e);
-              }
-            });
-          });
-        };
-
-        //check that every word has a result
-
-				
-        function buildScoresIfNotThere(words) {
-          if (words[0].score > 0) {
-            // assume all scores are there
-            return new Promise(function(resolve, reject) {
-              resolve(words);
-            });
-          }
-          //get all scores
-          return Promise.each(words, item => {
-            return getNumOfResults(item.name + " " + masterWord)
-              .then((score) => {
-                item.score = score;
-              })
-          }).then((words) => {
-            console.log("Built all word scores!");
-            words.sort(urlLib._evolutionModule.wordSorter);
-            return Promise.resolve(words);
-          })
-        };
-
-        buildScoresIfNotThere(words).then(function() {
-          console.log(JSON.stringify(words));
-
-          //get worst word in words
-          var worstWord = words.shift();
-          getNumOfResults(word + " " + masterWord).then(function(scoreNew) {
-            //give the oldWord a last chance
-            getNumOfResults(worstWord.name + " " + masterWord).then(function(scoreOld) {
-              //console.log("Fitnessfunction("+word+"): "+worstWord .name+" "+masterWord+": "+scoreOld+ " vs. "+word +" "+masterWord+": "+scoreNew);
-              if (scoreNew > scoreOld) {
-                words.push({
-                  name: word,
-                  score: scoreNew
+    // This is our main public function which returns the final approved url
+    generateURL: function (config) {
+        console.log("Received Config:");
+        console.log(config);
+        return new Promise((resolve, reject) => {
+            this._buildSearchString(config)
+                .then(this._getUrl)
+                .then(this._approveUrl)
+                .then((url) => {
+                    if (url) {
+                        resolve(url);
+                        return;
+                    }
+                    resolve(this.generateURL(config));
+                }).catch((err) => {
+                    console.log("Regenerate");
+                    //return this.generateURL(config);
                 });
-                console.log("Fitnessfunction: " + worstWord.name + " replaced by " + word + "(" + scoreNew + ")");
-              } else {
-                //update oldWord
-                worstWord.score = scoreOld;
-                words.push(worstWord);
-                console.log("Fitnessfunction: " + word + "(" + scoreNew + ")" + " was not able to replace " + worstWord.name);
-              }
-              words.sort(urlLib._evolutionModule.wordSorter);
+        })
+
+    },
+
+    //------------- evolutionary algorithm------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------
+
+    //-----fitness function--------------------------------------------------------------------------------
+    _evolutionModule: {
+        wordSorter: function (a, b) {
+            return a.score - b.score
+        },
+        updateLexicon: function (config) {
+            return new Promise(function (resolve, reject) {
+                //TODO: find new words (score can be zero in the first iteration)
+
+                var wordsWithScores = [{
+                    name: "Geld",
+                    score: 2
+                }, {
+                    name: "Finanzen",
+                    score: 0
+                }];
+                var personaKey = "Bank";
+                wordsWithScores.sort(urlLib._evolutionModule.wordSorter);
+                    //
+                 urlLib._evolutionModule.insertWordIfFitEnough("Holz", wordsWithScores, personaKey).then(function(d) {
+                     resolve(d);
+                 });
             });
-          });
+        },
+        insertWordIfFitEnough: function (word, words, masterWord) {
+
+            return new Promise(function (resolve, reject) {
+
+                var getNumOfResults = function (search) {
+                    return new Promise(function (resolve, reject) {
+                        var url = "https://www.google.com/search?q=" + encodeURIComponent(search);
+                        $.get({
+                            url: url,
+                            success: function (data) {
+                                var resultStatsRegexp = /<div id="resultStats">[^>]*</g;
+                                var numberRegex = /\d+/g;
+                                var resultStats = resultStatsRegexp.exec(data)[0].replace("<div id=\"resultStats\">", "").replace("<", "").replace(" Ergebnisse", "").replace(" Results", "").replace("Ungefähr ", "");
+                                var numberOfResults = resultStats.replace(/\./g, '');
+                                numberOfResults = numberRegex.exec(numberOfResults)[0];
+                                numberOfResults = parseInt(numberOfResults);
+
+                                resolve(numberOfResults);
+                            },
+                            error: function (e) {
+                                reject(e);
+                            }
+                        });
+                    });
+                };
+
+                //check that every word has a result
+
+
+                function buildScoresIfNotThere(words) {
+                    if (words[0].score > 0) {
+                        // assume all scores are there
+                        return new Promise(function (resolve, reject) {
+                            resolve(words);
+                        });
+                    }
+                    //get all scores
+                    return Promise.each(words, item => {
+                        return getNumOfResults(item.name + " " + masterWord)
+                            .then((score) => {
+                                item.score = score;
+                            })
+                    }).then((words) => {
+                        console.log("Built all word scores!");
+                        words.sort(urlLib._evolutionModule.wordSorter);
+                        return Promise.resolve(words);
+                    })
+                };
+
+                buildScoresIfNotThere(words).then(function () {
+                    console.log(JSON.stringify(words));
+
+                    //get worst word in words
+                    var worstWord = words.shift();
+                    getNumOfResults(word + " " + masterWord).then(function (scoreNew) {
+                        //give the oldWord a last chance
+                        getNumOfResults(worstWord.name + " " + masterWord).then(function (scoreOld) {
+                            //console.log("Fitnessfunction("+word+"): "+worstWord .name+" "+masterWord+": "+scoreOld+ " vs. "+word +" "+masterWord+": "+scoreNew);
+                            if (scoreNew > scoreOld) {
+                                words.push({
+                                    name: word,
+                                    score: scoreNew
+                                });
+                                console.log("Fitnessfunction: " + worstWord.name + " replaced by " + word + "(" + scoreNew + ")");
+                            } else {
+                                //update oldWord
+                                worstWord.score = scoreOld;
+                                words.push(worstWord);
+                                console.log("Fitnessfunction: " + word + "(" + scoreNew + ")" + " was not able to replace " + worstWord.name);
+                            }
+                            words.sort(urlLib._evolutionModule.wordSorter);
+                            resolve(words);
+                        });
+                    });
+                });
+
+            });
+        }
+    },
+
+
+    _buildSearchString(config) {
+        return new Promise(function (resolve, reject) {
+            console.log("build search string" + urlLib._evolutionModule);
+            urlLib._evolutionModule.updateLexicon(config).then(function (d) {
+                console.log("updated lexicon");
+                var allWords = d.map(function(e) {
+                    return e.name;
+                })
+                var searchString = allWords.join(" and ");
+                resolve(searchString);
+            });
         });
 
-      });
-    }
-  },
+        function _get_random_word(w) {
+            return w[Math.floor(Math.random() * w.length)];
+        }
+    },
 
-
-  _buildSearchString(config) {
-    return new Promise(function(resolve, reject) {
-      console.log("build search string" + urlLib._evolutionModule);
-      urlLib._evolutionModule.updateLexicon(config).then(function(config) {
-        console.log("updated lexicon");
-        var words = [];
-        words.push(_get_random_word(config.wordlist));
-        words.push(_get_random_word(config.wordlist));
-        var searchString = words.join(" and ");
-        resolve(searchString);
-      });
-    });
-
-    function _get_random_word(w) {
-      return w[Math.floor(Math.random() * w.length)];
-    }
-  },
-
-//------------- _getUrlModule Begin ------------------------------------------------------------------
+    //------------- _getUrlModule Begin ------------------------------------------------------------------
 
     _getUrlModule: {
         /**
@@ -156,15 +165,15 @@ var urlLib = {
          * @param page source of a google search
          * @return an array with all search results of this page, so usually 10 entries
          * */
-        _get_links: function(data) {
-            var result_link=/<div class="rc"><h3 class="r"><a href="([\S])+"/g;
-            var trim_html=/<div class="rc"><h3 class="r"><a href=/g;
-            var trim_ticks=/"/g;
+        _get_links: function (data) {
+            var result_link = /<div class="rc"><h3 class="r"><a href="([\S])+"/g;
+            var trim_html = /<div class="rc"><h3 class="r"><a href=/g;
+            var trim_ticks = /"/g;
             var all_links = [];
 
             do {
                 var match = result_link.exec(data);
-                if(match != null) {
+                if (match != null) {
                     //Extract only the link itself from the matched parts and store it in an array
                     all_links.push(match[0].replace(trim_html, "").replace(trim_ticks, ""));
                 }
@@ -178,15 +187,15 @@ var urlLib = {
          * the first shown result is clicked. Instead this simulates behaviour that some link on the first page is clicked.
          * @return a single URL of the search results
          * */
-        _get_random_URL: function(data) {
+        _get_random_URL: function (data) {
             var links = urlLib._getUrlModule._get_links(data);
             return links[Math.floor(Math.random() * links.length)];
         },
     },
 
-    _getUrl: function(searchString) {
+    _getUrl: function (searchString) {
         console.log("Search String is: " + searchString)
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
             var url = "https://www.google.com/search?q=" + encodeURIComponent(searchString);
             $.get({
                 url: url,
@@ -195,11 +204,7 @@ var urlLib = {
                     console.log("Created URL is: " + url);
                     resolve(url);
                 },
-                error: e =>{
-                    //var fake_test_data='lauzsdlazusbdöbö<div class="rc"><h3 class="r"><a href="LINK_A" onmousedown="...", ...>DESCRIPTION</a></h3>öanwdäöanwdän \
-                    // <div class="rc"><h3 class="r"><a href="http://www.getraenke-holz.de/getraenkewissen~8a81811d400f785601400f81c6fe0041.de.html" onmousedown="...", ...>DESCRIPTION</a></h3>öiuahwüdawüdawdüawdüawdn'
-                    //url = urlLib._getUrlModule._get_random_URL(fake_test_data);
-                    //console.log("Created URL from fake_test_data is: " + url);
+                error: e => {
                     reject(e);
                 }
             });
@@ -207,27 +212,28 @@ var urlLib = {
         })
     },
 
-//------------- _getUrlModule End --------------------------------------------------------------------
+    //------------- _getUrlModule End --------------------------------------------------------------------
 
 
-  // This Method must return the url if valid
-  // If the url is not valid the function mus return false
-  _approveUrl: function(url, config) {
-    return new Promise(function(resolve, reject) {
-      /*if (new Date().getTime() % 2) {
-        console.log("URL IS VALID");
-        resolve(url);
-        return;
-      }
-      console.log("URL IS NOT VALID");
-      resolve(false);*/
-       
-      resolve(url);
-      return;
+    // This Method must return the url if valid
+    // If the url is not valid the function mus return false
+    _approveUrl: function (url, config) {
+        return new Promise(function (resolve, reject) {
+            /*if (new Date().getTime() % 2) {
+              console.log("URL IS VALID");
+              resolve(url);
+              return;
+            }
+            console.log("URL IS NOT VALID");
+            resolve(false);*/
 
-    })
-  }
+            resolve(url);
+            return;
+
+        })
+    }
 
 
 
 };
+
