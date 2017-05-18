4890d9490d0eefc6b43878fb9c641a0b13d75389
bensteUEM <bensteUEM@users.noreply.github.com>
2017-05-18 10:49:56 +0200

    Revert "Changes from Chrome team on 18.05.2017"

--- 2-Chrome/contentScript.js
+++
@@ -1,9 +0,0 @@
-function pageLoaded() {
-	var arr = [], l = document.links;
-	for(var i=0; i<l.length; i++) {
-  		arr.push(l[i].href);
-	}
-	chrome.runtime.sendMessage({links: arr});
-}
-
-window.onload = pageLoaded;
