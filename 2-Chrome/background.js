5c67d65f43e460bd70599f26a21bd20f8f9b2c07
JuliKauf <jkaufmann23@googlemail.com>
2017-05-18 16:46:13 +0200

    Reactivate extension by settings

--- 2-Chrome/background.js
+++ 2-Chrome/background.js
@@ -184,6 +184,10 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
     if (changes.hasOwnProperty(active)) {
         let storageChange = changes[active];
         runMystique = storageChange.newValue
+		if (runMystique)
+		{
+			    run();
+		}
     }
 });
 
